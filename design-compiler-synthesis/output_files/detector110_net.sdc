2af3840bbf874a4d647cfbdff07d49e4afee09d3
Khanh N. Dang <dnk0904@gmail.com>
2023-11-23 21:36:04 +0900

    add design compiler

---
+++ design-compiler-synthesis/output_files/detector110_net.sdc
@@ -0,0 +1,14 @@
+###################################################################
+
+# Created by write_sdc on Thu Nov 23 21:35:23 2023
+
+###################################################################
+set sdc_version 2.1
+
+set_units -time ns -resistance MOhm -capacitance fF -voltage V -current mA
+set_wire_load_model -name 5K_hvratio_1_1 -library NangateOpenCellLibrary
+create_clock [get_ports clk]  -period 2  -waveform {0 1}
+set_clock_uncertainty 0.004  [get_clocks clk]
+set_input_delay -clock clk  0.1  [get_ports a]
+set_input_delay -clock clk  0.1  [get_ports reset]
+set_output_delay -clock clk  0.1  [get_ports w]
---
+++ design-compiler-synthesis/output_files/detector110_net.v
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////////////////
+// Created by: Synopsys DC Expert(TM) in wire load mode
+// Version   : Q-2019.12-SP4
+// Date      : Thu Nov 23 21:35:23 2023
+/////////////////////////////////////////////////////////////
+
+
+module detector110 ( a, clk, reset, w );
+  input a, clk, reset;
+  output w;
+  wire   N18, N19, N20, n1, n2, n3, n4, n5, n6;
+  wire   [1:0] state;
+  assign w = N20;
+
+  DFF_X1 \state_reg[0]  ( .D(N18), .CK(clk), .Q(state[0]), .QN(n2) );
+  DFF_X1 \state_reg[1]  ( .D(N19), .CK(clk), .Q(state[1]), .QN(n1) );
+  NAND3_X1 U9 ( .A1(state[0]), .A2(n1), .A3(a), .ZN(n4) );
+  NOR2_X1 U10 ( .A1(n1), .A2(n2), .ZN(N20) );
+  AOI21_X1 U11 ( .B1(n3), .B2(n4), .A(reset), .ZN(N19) );
+  NAND2_X1 U12 ( .A1(state[1]), .A2(n2), .ZN(n3) );
+  NOR2_X1 U13 ( .A1(reset), .A2(n5), .ZN(N18) );
+  AOI22_X1 U14 ( .A1(n6), .A2(n2), .B1(N20), .B2(a), .ZN(n5) );
+  XNOR2_X1 U15 ( .A(n1), .B(a), .ZN(n6) );
+endmodule
+
